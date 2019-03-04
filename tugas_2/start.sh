9f2f495bf41c7d170165b7f8bc3cd2b74672b71c
William Adjandra <willihogan@gmail.com>
2019-03-04 12:28:00 +0700

    change start.sh

--- tugas_2/start.sh
+++ tugas_2/start.sh
@@ -2,6 +2,6 @@
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
-exec gunicorn testRest.wsgi:application \
+exec gunicorn tugas_2.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3
